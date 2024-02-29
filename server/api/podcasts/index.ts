ac8628f4aaffe24b75c9e7114d875c392c5d1383
MohammadAli Khaksar <mohammadali.khaksar.79@gmail.com>
2024-02-29 23:20:03 +0330

    remove unused packages, composables, add filters to posts, podcasts files in server directory

--- server/api/podcasts/index.ts
+++ server/api/podcasts/index.ts
@@ -2,9 +2,24 @@ import { podcastsEn, podcastsFa } from "~/utilities/fakeData";
 import { Podcast } from "~/utilities/types";
 
 export default defineEventHandler(async (event) => {
-  const { locale } = await getQuery(event);
+  const { locale, filterKey } = await getQuery(event);
   const podcasts: Podcast[] =
     locale && locale === "fa" ? podcastsFa : podcastsEn;
 
-  return podcasts;
+  switch (filterKey) {
+    case "newest":
+      return podcasts.sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      );
+    case "most-popular":
+      return podcasts.sort((a, b) => b.likes - a.likes);
+    case "oldest":
+      return podcasts.sort(
+        (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+      );
+    default:
+      return podcasts.sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      );
+  }
 });
--- server/api/posts/index.ts
+++ server/api/posts/index.ts
@@ -2,8 +2,23 @@ import { postsEn, postsFa } from "~/utilities/fakeData";
 import { Post } from "~/utilities/types";
 
 export default defineEventHandler(async (event) => {
-  const { locale } = await getQuery(event);
+  const { locale, filterKey } = await getQuery(event);
   const posts: Post[] = locale && locale === "fa" ? postsFa : postsEn;
 
-  return posts;
+  switch (filterKey) {
+    case "newest":
+      return posts.sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      );
+    case "most-popular":
+      return posts.sort((a, b) => b.likes - a.likes);
+    case "oldest":
+      return posts.sort(
+        (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+      );
+    default:
+      return posts.sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      );
+  }
 });
